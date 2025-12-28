94b21be72e9852b1b29bf25db9766e8f49a96d3d
Nitrous <tukheo123@gmail.com>
2025-12-28 10:25:24 +0700

    Update

--- demo/image_matting/colab/inference.py
+++ demo/image_matting/colab/inference.py
@@ -1,48 +1,38 @@
+# inference.py
 import os
 import sys
 import argparse
 import numpy as np
 from PIL import Image
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-
 from src.models.modnet import MODNet
 
-
 if __name__ == '__main__':
-    # define cmd arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', type=str, help='path of input images')
+    parser.add_argument('--input-path', type=str, help='path of input images or a single image file')
     parser.add_argument('--output-path', type=str, help='path of output images')
     parser.add_argument('--ckpt-path', type=str, help='path of pre-trained MODNet')
     args = parser.parse_args()
 
-    # check input arguments
     if not os.path.exists(args.input_path):
-        print('Cannot find input path: {0}'.format(args.input_path))
+        print(f'Cannot find input path: {args.input_path}')
         exit()
     if not os.path.exists(args.output_path):
-        print('Cannot find output path: {0}'.format(args.output_path))
+        print(f'Cannot find output path: {args.output_path}')
         exit()
     if not os.path.exists(args.ckpt_path):
-        print('Cannot find ckpt path: {0}'.format(args.ckpt_path))
+        print(f'Cannot find checkpoint path: {args.ckpt_path}')
         exit()
 
-    # define hyper-parameters
     ref_size = 512
 
-    # define image to tensor transform
     im_transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ]
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
 
-    # create MODNet and load the pre-trained ckpt
     modnet = MODNet(backbone_pretrained=False)
     modnet = nn.DataParallel(modnet)
 
@@ -51,18 +41,20 @@ if __name__ == '__main__':
         weights = torch.load(args.ckpt_path)
     else:
         weights = torch.load(args.ckpt_path, map_location=torch.device('cpu'))
+
     modnet.load_state_dict(weights)
     modnet.eval()
 
-    # inference images
-    im_names = os.listdir(args.input_path)
+    if os.path.isdir(args.input_path):
+        im_names = os.listdir(args.input_path)
+    else:
+        im_names = [os.path.basename(args.input_path)]
+
     for im_name in im_names:
-        print('Process image: {0}'.format(im_name))
+        full_path = os.path.join(args.input_path, im_name) if os.path.isdir(args.input_path) else args.input_path
+        print(f'Processing image: {full_path}')
 
-        # read image
-        im = Image.open(os.path.join(args.input_path, im_name))
-
-        # unify image channels to 3
+        im = Image.open(full_path)
         im = np.asarray(im)
         if len(im.shape) == 2:
             im = im[:, :, None]
@@ -71,35 +63,37 @@ if __name__ == '__main__':
         elif im.shape[2] == 4:
             im = im[:, :, 0:3]
 
-        # convert image to PyTorch tensor
-        im = Image.fromarray(im)
-        im = im_transform(im)
-
-        # add mini-batch dim
+        im = im_transform(Image.fromarray(im))
         im = im[None, :, :, :]
 
-        # resize image for input
         im_b, im_c, im_h, im_w = im.shape
         if max(im_h, im_w) < ref_size or min(im_h, im_w) > ref_size:
             if im_w >= im_h:
                 im_rh = ref_size
                 im_rw = int(im_w / im_h * ref_size)
-            elif im_w < im_h:
+            else:
                 im_rw = ref_size
                 im_rh = int(im_h / im_w * ref_size)
         else:
             im_rh = im_h
             im_rw = im_w
-        
+
         im_rw = im_rw - im_rw % 32
         im_rh = im_rh - im_rh % 32
         im = F.interpolate(im, size=(im_rh, im_rw), mode='area')
 
-        # inference
         _, _, matte = modnet(im.cuda() if torch.cuda.is_available() else im, True)
-
-        # resize and save matte
         matte = F.interpolate(matte, size=(im_h, im_w), mode='area')
         matte = matte[0][0].data.cpu().numpy()
-        matte_name = im_name.split('.')[0] + '.png'
-        Image.fromarray(((matte * 255).astype('uint8')), mode='L').save(os.path.join(args.output_path, matte_name))
+
+        original_im = np.asarray(Image.open(full_path))
+        if original_im.shape[2] == 4:
+            original_im = original_im[:, :, :3]
+
+        foreground = np.zeros((original_im.shape[0], original_im.shape[1], 4), dtype=np.uint8)
+        foreground[..., :3] = original_im
+        foreground[..., 3] = (matte * 255).astype(np.uint8)
+
+        foreground_name = im_name.split('.')[0] + '_foreground.png'
+        Image.fromarray(foreground).save(os.path.join(args.output_path, foreground_name), format='PNG')
+        print(f'Saved foreground image: {foreground_name}')
